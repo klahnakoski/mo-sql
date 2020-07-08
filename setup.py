d992fdb159eb2db07117108c41a833c3acff7e73
Kyle Lahnakoski <kyle@lahnakoski.com>
2020-07-08 07:12:27 -0400

    update version number

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
     classifiers=["Development Status :: 4 - Beta","Topic :: Software Development :: Libraries","Topic :: Software Development :: Libraries :: Python Modules","License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)","Programming Language :: Python :: 2.7","Programming Language :: Python :: 3.6","Programming Language :: Python :: 3.7","Programming Language :: Python :: 3.8"],
     description='More SQL!  For safely assembling SQL',
     include_package_data=True,
-    install_requires=["mo-dots>=3.53.20059","mo-future>=3.51.20059","mo-logs>=3.53.20059"],
+    install_requires=["mo-dots>=3.77.20190","mo-future>=3.71.20168","mo-logs>=3.75.20189"],
     license='MPL 2.0',
     long_description='# mo-sql\n\nMore SQL!\n\nA SQL datatype, plus a number of constants for safe SQL composition.',
     long_description_content_type='text/markdown',
     name='mo-sql',
     packages=["mo_sql"],
     url='https://github.com/klahnakoski/mo-sql',
-    version='3.55.20074'
+    version='3.77.20190'
 )
